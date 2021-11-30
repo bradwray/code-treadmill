3125ada047dcf0c289c4b12e8451fb221b1af923
Wray <bwray@bcps.org>
2021-11-30 13:45:01 -0500

    situated the bars of the progress chart to scale dependent on the number of slides in the workout

--- components/AppContext.js
+++ components/AppContext.js
@@ -1,22 +1,25 @@
-import React, { createContext } from 'react';
+import React, { createContext } from "react";
+import { slides } from "../codeUtils/jsCodeContent.js";
 
-import { ThemeProvider } from 'styled-components';
-import cobalt2 from '../codeUtils/cobalt.cjs.js';
+import { ThemeProvider } from "styled-components";
+import cobalt2 from "../codeUtils/cobalt.cjs.js";
 
 export const Context = createContext();
 
 export function AppContext(props) {
-   const [store, setStore] = React.useState({
-      readStats: [],
-      rpm: 0,
-      avgDifficulty: 0,
-      theme: cobalt2,
-      themeName: 'cobalt2',
-   });
-   // console.log(store.theme);
-   return (
-      <ThemeProvider theme={store.theme}>
-         <Context.Provider value={[store, setStore]}>{props.children}</Context.Provider>
-      </ThemeProvider>
-   );
+  const [store, setStore] = React.useState({
+    slides: slides,
+    rpm: 0,
+    avgDifficulty: 0,
+    theme: cobalt2,
+    themeName: "cobalt2",
+  });
+  // console.log(store.theme);
+  return (
+    <ThemeProvider theme={store.theme}>
+      <Context.Provider value={[store, setStore]}>
+        {props.children}
+      </Context.Provider>
+    </ThemeProvider>
+  );
 }
