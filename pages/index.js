50e8afa7cce5e6bc63496072d09aa4322ed7d94d
bradwray <bradwray@gmail.com>
2021-12-02 14:52:55 -0500

    attempting a fix on scaling and zooming that will work for both safari and chrome.

--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,36 @@
-import { AppContext } from '../components/AppContext';
-import Panel from '../components/Panel';
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { StrictMode } from 'react';
-import TreadMill from '../components/TreadMill';
-import styled from 'styled-components';
+import { AppContext } from "../components/AppContext";
+import Panel from "../components/Panel";
+import React from "react";
+import ReactDOM from "react-dom";
+import { StrictMode } from "react";
+import TreadMill from "../components/TreadMill";
+import styled from "styled-components";
+import Head from "next/head";
 
 const Wrapper = styled.div`
-   overflow: hidden;
-   height: 100vh;
-   width: 100vw;
-   /* margin: -20px; */
-   background: ${(props) => props.theme.plain.backgroundColor + 'bb'};
-   display: flex;
-   justify-content: center;
+  overflow: hidden;
+  height: 100vh;
+  width: 100vw;
+  /* margin: -20px; */
+  background: ${(props) => props.theme.plain.backgroundColor + "bb"};
+  display: flex;
+  justify-content: center;
 `;
 
 export default function App() {
-   return (
-      <AppContext>
-         <Wrapper>
-            <Panel />
-            <TreadMill />
-         </Wrapper>
-      </AppContext>
-   );
+  return (
+    <AppContext>
+      <Head>
+        <title>codeRPM</title>
+        <meta
+          name="viewport"
+          content="initial-scale=1.0, width=device-width maximum-scale=1"
+        />
+      </Head>
+      <Wrapper>
+        <Panel />
+        <TreadMill />
+      </Wrapper>
+    </AppContext>
+  );
 }
