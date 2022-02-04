a3788c78358f0da31b9ea2ac8bacca96c6edb911
Brad Wray <bradwray@gmail.com>
2022-02-04 15:22:52 -0500

    got the leaderboard live relaoding correclty through socket io emits etc.

--- components/AppContext.js
+++ components/AppContext.js
@@ -1,60 +1,58 @@
-import React, { createContext, useEffect } from "react";
+import React, { createContext, useEffect } from 'react';
 
-import { ThemeProvider } from "styled-components";
-import io from "socket.io-client";
-import nightOwl from "../themes/nightOwl.cjs.js";
-import slides from "../workouts/variables";
-import tagAndWeightCode from "../utils/tagAndWeightCode.js";
+import { ThemeProvider } from 'styled-components';
+import io from 'socket.io-client';
+import nightOwl from '../themes/nightOwl.cjs.js';
+import slides from '../workouts/variables';
+import tagAndWeightCode from '../utils/tagAndWeightCode.js';
 
 const socket = io();
 
 export const Context = createContext();
 
 export function AppContext(props) {
-  const [store, setStore] = React.useState({
-    currentIndex: 0,
-    readStats: [],
-    raceStats: [
-      {
-        user: "Brad",
-        rpm: 2,
-        avgComplexity: 7,
-      },
-    ],
-    slides: tagAndWeightCode(slides),
-    workout: "",
-    rpm: 0,
-    avgComplexity: 0,
-    theme: nightOwl,
-    themeName: "nightOwl",
-    themeNum: 0,
-    leftAligned: true,
-    score: 0,
-  });
-  //   console.log(store);
-  useEffect(() => {
-    //the linter told me to do it this way
-    async function importWorkout() {
-      const workoutExercises = await import("../workouts/" + props.route);
-      setStore({
-        ...store,
-        workout: props.route,
-        slides: tagAndWeightCode(workoutExercises.default),
-        leftAligned: window.innerWidth < 900 ? false : true,
-      });
-    }
-    if (props.route) {
-      if (props.route !== store.workout) {
-        importWorkout();
+   const [store, setStore] = React.useState({
+      currentIndex: 0,
+      readStats: [],
+      raceStats: [
+         {
+            user: 'Brad',
+            rpm: 2,
+            avgComplexity: 7,
+         },
+      ],
+      slides: tagAndWeightCode(slides),
+      workout: '',
+      rpm: 0,
+      avgComplexity: 0,
+      theme: nightOwl,
+      themeName: 'nightOwl',
+      themeNum: 0,
+      leftAligned: true,
+      score: 0,
+   });
+   //   console.log(store);
+   useEffect(() => {
+      //the linter told me to import dynamic routes this way
+      async function importWorkout() {
+         const workoutExercises = await import('../workouts/' + props.route);
+         setStore({
+            ...store,
+            workout: props.route,
+            slides: tagAndWeightCode(workoutExercises.default),
+            leftAligned: window.innerWidth < 900 ? false : true,
+         });
       }
-    }
-  }, [props.route]);
+      if (props.route) {
+         if (props.route !== store.workout) {
+            importWorkout();
+         }
+      }
+   }, [props.route]);
 
-  return (
-    <ThemeProvider theme={store.theme}>
-      <Context.Provider value={[store, setStore]}>
-        {props.children}
-      </Context.Provider>
-    </ThemeProvider>
-  );
+   return (
+      <ThemeProvider theme={store.theme}>
+         <Context.Provider value={[store, setStore]}>{props.children}</Context.Provider>
+      </ThemeProvider>
+   );
 }
--- components/LeaderBoard.js
+++ components/LeaderBoard.js
@@ -1,117 +1,129 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { useContext, useEffect, useRef, useState } from 'react';
 
-import Button from "./Button";
-import { Context } from "./AppContext";
-import io from "socket.io-client";
-import styled from "styled-components";
+import Button from './Button';
+import { Context } from './AppContext';
+import io from 'socket.io-client';
+import styled from 'styled-components';
 
 const socket = io();
 
 const BoardContainer = styled.div`
-  width: 380px;
-  /* height: 124px; */
-  font-family: "Orbitron", sans-serif;
-  border: 1px solid #666;
-  border-radius: 10px;
-  color: ${(props) => props.theme.styles[5].style.color};
-  background-color: ${(props) => props.theme.plain.backgroundColor};
-  padding-top: 8px;
-  box-shadow: inset 0px 0px 2px 0px white;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  margin: 10px;
-  display: ${(props) => (!props.leftAligned ? `none` : ``)};
-  @media (max-width: 768px) {
-    display: none;
-  }
+   width: 380px;
+   /* height: 124px; */
+   font-family: 'Orbitron', sans-serif;
+   border: 1px solid #666;
+   border-radius: 10px;
+   color: ${(props) => props.theme.styles[5].style.color};
+   background-color: ${(props) => props.theme.plain.backgroundColor};
+   padding-top: 8px;
+   box-shadow: inset 0px 0px 2px 0px white;
+   display: flex;
+   flex-direction: column;
+   align-items: center;
+   margin: 10px;
+   display: ${(props) => (!props.leftAligned ? `none` : ``)};
+   @media (max-width: 768px) {
+      display: none;
+   }
 `;
 
 const InputBox = styled.input`
-  bottom: 25px;
-  width: ${(props) => (props.w ? `200px` : `60px`)};
-  background-color: ${(props) => props.theme.plain.backgroundColor};
-  border-color: ${(props) => props.theme.plain.color + "66"};
-  border-radius: 2px;
-  filter: brightness(130%);
-  color: ${(props) => props.theme.plain.color};
-  height: 30px;
-  text-align: center;
+   bottom: 25px;
+   width: ${(props) => (props.w ? `200px` : `60px`)};
+   background-color: ${(props) => props.theme.plain.backgroundColor};
+   border-color: ${(props) => props.theme.plain.color + '66'};
+   border-radius: 2px;
+   filter: brightness(130%);
+   color: ${(props) => props.theme.plain.color};
+   height: 30px;
+   text-align: center;
 `;
 
 const Row = styled.tr`
-  width: 200px;
-  display: flex;
-  justify-content: center;
+   width: 200px;
+   display: flex;
+   justify-content: center;
+`;
+
+const YouRow = styled(Row)`
+   border: 2px solid ${(props) => props.theme.plain.color};
+   border-radius: 2px;
 `;
 
 const Cell = styled.td`
-  min-width: 60px;
-  text-align: left;
-  margin-right: 20px;
+   min-width: 60px;
+   text-align: left;
+   margin-right: 20px;
 `;
 
 const LeaderBoard = ({}) => {
-  const [store, setStore] = useContext(Context);
-  const [results, setResults] = useState({});
-  const [userInfo, setUserInfo] = useState({ name: "", joined: false });
+   const [store, setStore] = useContext(Context);
+   const [results, setResults] = useState({});
+   const nameInput = useRef();
 
-  useEffect(() => {
-    fetch("/api/socketio").finally(() => {
-      socket.on("updateRace", (raceStats) => {
-        console.log("client raceStats");
-        setResults(raceStats);
-      });
+   useEffect(() => {
+      console.log(socket);
+      if (!socket.connected) {
+         fetch('/api/socketio').finally(() => {
+            socket.on('updateRace', (raceStats) => {
+               console.log('updateRace');
+               setResults(raceStats);
+            });
 
-      socket.on("a user connected", () => {
-        console.log("a user connected?");
-      });
-    });
-    const yourNewScore = {
-      name: userInfo.name,
-      score: store.score,
-      progress: ((store.currentIndex / store.slides.length) * 100).toFixed(0),
-    };
+            socket.on('a user connected', () => {
+               console.log('a user connected?');
+            });
+         });
+      }
+   }, [store.currentIndex]);
 
-    socket.emit("newResult", yourNewScore);
-    let temp = results;
-    temp[results.name] = yourNewScore;
-    setResults(temp);
-  }, [store.currentIndex]);
+   const handleJoin = () => {
+      setStore({ ...store, userName: nameInput.current.value });
+      const youUser = {
+         name: nameInput.current.value,
+         score: 0,
+         progress: 0,
+      };
+      socket.emit('newResult', youUser);
+      let temp = results;
+      temp[nameInput.current.value] = youUser;
+      setResults(temp);
+   };
 
-  const handleJoin = () => {
-    setUserInfo({ ...userInfo, joined: true });
-    socket.emit("newResult", {
-      name: userInfo.name,
-      score: 0,
-      progress: 0,
-    });
-  };
-  const resultsList = Object.keys(results)
-    .map((competitorKey, i) => results[competitorKey])
-    .sort((a, b) => b.score * b.progress - a.score * a.progress);
-  return (
-    <BoardContainer leftAligned={store.leftAligned}>
-      {!userInfo.joined ? (
-        <div>
-          <InputBox
-            onChange={(e) => setUserInfo({ name: e.target.value })}
-          ></InputBox>
-          <Button onClick={() => handleJoin()}>Join</Button>
-        </div>
-      ) : null}
-      {resultsList.map((competitor, i) => {
-        return (
-          <Row key={i}>
-            <Cell>{i + 1 + ": "}</Cell>
-            <Cell>{competitor.name}</Cell>
-            <Cell>{competitor.score}</Cell>
-            <Cell>{competitor.progress}</Cell>
-          </Row>
-        );
-      })}
-    </BoardContainer>
-  );
+   const resultsList = Object.keys(results)
+      .map((competitorKey, i) => results[competitorKey])
+      .sort((a, b) => b.score * b.progress - a.score * a.progress);
+   return (
+      <BoardContainer leftAligned={store.leftAligned}>
+         {!results.hasOwnProperty(store.userName) ? (
+            <div>
+               <InputBox type='text' ref={nameInput}></InputBox>
+               <Button onClick={() => handleJoin()}>Join</Button>
+            </div>
+         ) : null}
+         {resultsList.map((competitor, i) => {
+            if (competitor.name == store.userName) {
+               return (
+                  <YouRow key={i}>
+                     <Cell>{i + 1 + ': '}</Cell>
+                     <Cell>{competitor.name}</Cell>
+                     <Cell>{competitor.score}</Cell>
+                     <Cell>{competitor.progress}</Cell>
+                  </YouRow>
+               );
+            } else {
+               return (
+                  <Row key={i}>
+                     <Cell>{i + 1 + ': '}</Cell>
+                     <Cell>{competitor.name}</Cell>
+                     <Cell>{competitor.score}</Cell>
+                     <Cell>{competitor.progress}</Cell>
+                  </Row>
+               );
+            }
+         })}
+      </BoardContainer>
+   );
 };
 
 export default LeaderBoard;
