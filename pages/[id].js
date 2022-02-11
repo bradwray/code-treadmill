9e66ffd6fc5adb5aa4ff892472325fee15ef9278
Brad Wray <bradwray@gmail.com>
2022-02-10 21:26:46 -0500

    changed name. Way To Code

--- pages/[id].js
+++ pages/[id].js
@@ -1,41 +1,42 @@
-import { AppContext } from "../components/AppContext";
-import Panel from "../components/Panel";
-import React, { useContext, useEffect } from "react";
-import TreadMill from "../components/TreadMill";
-import styled from "styled-components";
-import { useRouter } from "next/router";
-import Head from "next/head";
+import React, { useContext, useEffect } from 'react';
+
+import { AppContext } from '../components/AppContext';
+import Head from 'next/head';
+import Panel from '../components/Panel';
+import TreadMill from '../components/TreadMill';
+import styled from 'styled-components';
+import { useRouter } from 'next/router';
 
 const Wrapper = styled.div`
-  overflow: hidden;
-  height: 100vh;
-  width: 100vw;
-  background: ${(props) => props.theme.plain.backgroundColor + "bb"};
-  display: flex;
-  justify-content: center;
+   overflow: hidden;
+   height: 100vh;
+   width: 100vw;
+   background: ${(props) => props.theme.plain.backgroundColor + 'bb'};
+   display: flex;
+   justify-content: center;
 `;
 
 export default function App({}) {
-  const router = useRouter();
-  const { id } = router.query;
+   const router = useRouter();
+   const { id } = router.query;
 
-  if (id) {
-    return (
-      <AppContext route={id}>
-        <Head>
-          <title>codeRPM</title>
-          <meta
-            name="viewport"
-            content="initial-scale=1.0, width=device-width maximum-scale=1"
-          />
-        </Head>
-        <Wrapper>
-          <Panel />
-          <TreadMill />
-        </Wrapper>
-      </AppContext>
-    );
-  } else {
-    return null;
-  }
+   if (id) {
+      return (
+         <AppContext route={id}>
+            <Head>
+               <title>Way-To-Code {id}</title>
+               <meta
+                  name='viewport'
+                  content='initial-scale=1.0, width=device-width maximum-scale=1'
+               />
+            </Head>
+            <Wrapper>
+               <Panel />
+               <TreadMill />
+            </Wrapper>
+         </AppContext>
+      );
+   } else {
+      return null;
+   }
 }
--- pages/index.js
+++ pages/index.js
@@ -1,33 +1,30 @@
-import { AppContext } from "../components/AppContext";
-import Panel from "../components/Panel";
-import React from "react";
-import TreadMill from "../components/TreadMill";
-import styled from "styled-components";
-import Head from "next/head";
+import { AppContext } from '../components/AppContext';
+import Head from 'next/head';
+import Panel from '../components/Panel';
+import React from 'react';
+import TreadMill from '../components/TreadMill';
+import styled from 'styled-components';
 
 const Wrapper = styled.div`
-  overflow: hidden;
-  height: 100vh;
-  width: 100vw;
-  background: ${(props) => props.theme.plain.backgroundColor + "bb"};
-  display: flex;
-  justify-content: center;
+   overflow: hidden;
+   height: 100vh;
+   width: 100vw;
+   background: ${(props) => props.theme.plain.backgroundColor + 'bb'};
+   display: flex;
+   justify-content: center;
 `;
 
 export default function App() {
-  return (
-    <AppContext>
-      <Head>
-        <title>codeRPM</title>
-        <meta
-          name="viewport"
-          content="initial-scale=1.0, width=device-width maximum-scale=1"
-        />
-      </Head>
-      <Wrapper>
-        <Panel />
-        <TreadMill />
-      </Wrapper>
-    </AppContext>
-  );
+   return (
+      <AppContext>
+         <Head>
+            <title>Way-To-Code</title>
+            <meta name='viewport' content='initial-scale=1.0, width=device-width maximum-scale=1' />
+         </Head>
+         <Wrapper>
+            <Panel />
+            <TreadMill />
+         </Wrapper>
+      </AppContext>
+   );
 }
