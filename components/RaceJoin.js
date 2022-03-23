b02d8c72c4124cbc23e35c003bc57cf188c8f9be
bradwray <bradwray@gmail.com>
2022-03-23 10:45:37 -0400

    found problem in racejoin where workout wasn't being set to the raceRoute. Seeing how it works in production

--- components/RaceJoin.js
+++ components/RaceJoin.js
@@ -1,145 +1,152 @@
-import React, { useContext, useEffect, useState } from 'react';
+import React, { useContext, useEffect, useState } from "react";
 
-import Button from './Button';
-import CardContainer from './CardContainer';
-import { Context } from './AppContext';
-import LeaderBoard from './LeaderBoard';
-import Link from './Link';
-import Title from './Title';
-import io from 'socket.io-client';
-import styled from 'styled-components';
-import { useRouter } from 'next/router';
+import Button from "./Button";
+import CardContainer from "./CardContainer";
+import { Context } from "./AppContext";
+import LeaderBoard from "./LeaderBoard";
+import Link from "./Link";
+import Title from "./Title";
+import io from "socket.io-client";
+import styled from "styled-components";
+import { useRouter } from "next/router";
 
 const socket = io();
 const Section = styled.div`
-   border: 1px solid ${(props) => props.theme.plain.color + '66'};
-   padding: 30px;
-   margin: 25px;
-   width: 100%;
-   max-width: 750px;
-   min-height: 450px;
+  border: 1px solid ${(props) => props.theme.plain.color + "66"};
+  padding: 30px;
+  margin: 25px;
+  width: 100%;
+  max-width: 750px;
+  min-height: 450px;
 `;
 
 const Btn = styled(Button)`
-   display: block;
-   bottom: 0px;
-   right: 0px;
-   margin-top: 10%;
-   margin-left: 80%;
+  display: block;
+  bottom: 0px;
+  right: 0px;
+  margin-top: 10%;
+  margin-left: 80%;
 `;
 
 const Input = styled.input`
-   display: block;
-   width: 700px;
-   text-align: center;
-   margin: 0 auto;
-   margin-top: 80px;
-   border-radius: 5px;
-   border: 2px solid ${(props) => props.theme.plain.color};
-   color: ${(props) => props.theme.styles[2].style.color};
-   font-size: 80px;
+  display: block;
+  width: 700px;
+  text-align: center;
+  margin: 0 auto;
+  margin-top: 80px;
+  border-radius: 5px;
+  border: 2px solid ${(props) => props.theme.plain.color};
+  color: ${(props) => props.theme.styles[2].style.color};
+  font-size: 80px;
 `;
 
 const RaceJoin = ({}) => {
-   const router = useRouter();
-   const [store, setStore] = useContext(Context);
-   const [state, setState] = useState({
-      joined: false,
-      joinCode: '',
-      name: '',
-   });
-   useEffect(() => {
-      if (!socket.connected) {
-         fetch('/api/socketio').finally(() => {
-            // socket.on(store.raceID + '-raceStart', (raceRoute) => {
-            //    router.push(raceRoute);
-            //    setStore({ ...store, rpm: 0, score: 0, currentIndex: 0 });
-            // });
+  const router = useRouter();
+  const [store, setStore] = useContext(Context);
+  const [state, setState] = useState({
+    joined: false,
+    joinCode: "",
+    name: "",
+  });
+  useEffect(() => {
+    if (!socket.connected) {
+      fetch("/api/socketio").finally(() => {
+        // socket.on(store.raceID + '-raceStart', (raceRoute) => {
+        //    router.push(raceRoute);
+        //    setStore({ ...store, rpm: 0, score: 0, currentIndex: 0 });
+        // });
 
-            socket.on('a user connected', () => {
-               console.log('a user connected?');
-            });
-         });
-      } else {
-         socket.on('welcome', (userName) => {
-            if (userName === store.userName) {
-               setState({ ...state, joined: true, message: null });
+        socket.on("a user connected", () => {
+          console.log("a user connected?");
+        });
+      });
+    } else {
+      socket.on("welcome", (userName) => {
+        if (userName === store.userName) {
+          setState({ ...state, joined: true, message: null });
+        }
+      });
+      socket.on("woops", (userName) => {
+        if (userName === store.userName) {
+          setState({ ...state, joined: false, message: "Invalid join code" });
+        }
+      });
+      socket.on("raceBegan", (raceID, startTime, raceRoute) => {
+        if (store.raceID === raceID) {
+          setStore({
+            ...store,
+            startTime,
+            raceWorkout: raceRoute,
+            workout: raceRoute,
+            score: 0,
+            currentIndex: 0,
+            progress: 0,
+          });
+          router.push(
+            "/" + raceRoute + "?raceID=" + raceID + "&uName=" + store.userName
+          );
+        }
+      });
+    }
+  }, [store.raceID, store.userName]);
+
+  const handleJoin = () => {
+    //this next line ensures unique results among same named competitors
+    const userName =
+      state.name + "~~" + Math.floor(Math.random() * 100).toString();
+    setState({ ...state, joining: true, message: "Joining..." });
+    setStore({ ...store, raceID: state.joinCode, userName: userName });
+
+    socket.emit("joinRace", state.joinCode, userName);
+  };
+  const handleTextEntry = (val, box) => {
+    if (box === "code") {
+      setState({ ...state, joinCode: val });
+    } else {
+      setState({ ...state, name: val });
+    }
+  };
+
+  return (
+    <CardContainer>
+      {state.joined ? (
+        <Section>Wait for the race to begin. Stay on this page.</Section>
+      ) : (
+        <Section>
+          <Title w="180px" xOffset="-39px">
+            Join Race
+          </Title>
+          Enter your name and a join code.
+          <Input
+            onChange={(e) => handleTextEntry(e.target.value, "name")}
+            autoFocus
+            placeholder="your name"
+          />
+          <Input
+            onChange={(e) => handleTextEntry(e.target.value, "code")}
+            placeholder="join code"
+          />
+          {state.message ? state.message : null}
+          <Btn
+            disabled={
+              state.name == "" ||
+              state.joinCode == "" ||
+              state.joining == "Joining..."
             }
-         });
-         socket.on('woops', (userName) => {
-            if (userName === store.userName) {
-               setState({ ...state, joined: false, message: 'Invalid join code' });
-            }
-         });
-         socket.on('raceBegan', (raceID, startTime, raceRoute) => {
-            if (store.raceID === raceID) {
-               setStore({
-                  ...store,
-                  startTime,
-                  raceWorkout: raceRoute,
-                  score: 0,
-                  currentIndex: 0,
-                  progress: 0,
-               });
-               router.push('/' + raceRoute + '?raceID=' + raceID + '&uName=' + store.userName);
-            }
-         });
-      }
-   }, [store.raceID, store.userName]);
+            onClick={() => handleJoin()}
+          >
+            Join Race
+          </Btn>
+          Or...
+          <Link ownLine target="_blank" href="/race-manager">
+            Create your own race and invite others
+          </Link>
+        </Section>
+      )}
 
-   const handleJoin = () => {
-      //this next line ensures unique results among same named competitors
-      const userName = state.name + '~~' + Math.floor(Math.random() * 100).toString();
-      setState({ ...state, joining: true, message: 'Joining...' });
-      setStore({ ...store, raceID: state.joinCode, userName: userName });
-
-      socket.emit('joinRace', state.joinCode, userName);
-   };
-   const handleTextEntry = (val, box) => {
-      if (box === 'code') {
-         setState({ ...state, joinCode: val });
-      } else {
-         setState({ ...state, name: val });
-      }
-   };
-
-   return (
-      <CardContainer>
-         {state.joined ? (
-            <Section>Wait for the race to begin. Stay on this page.</Section>
-         ) : (
-            <Section>
-               <Title w='180px' xOffset='-39px'>
-                  Join Race
-               </Title>
-               Enter your name and a join code.
-               <Input
-                  onChange={(e) => handleTextEntry(e.target.value, 'name')}
-                  autoFocus
-                  placeholder='your name'
-               />
-               <Input
-                  onChange={(e) => handleTextEntry(e.target.value, 'code')}
-                  placeholder='join code'
-               />
-               {state.message ? state.message : null}
-               <Btn
-                  disabled={
-                     state.name == '' || state.joinCode == '' || state.joining == 'Joining...'
-                  }
-                  onClick={() => handleJoin()}>
-                  Join Race
-               </Btn>
-               Or...
-               <Link ownLine target='_blank' href='/race-manager'>
-                  Create your own race and invite others
-               </Link>
-            </Section>
-         )}
-
-         <LeaderBoard />
-      </CardContainer>
-   );
+      <LeaderBoard />
+    </CardContainer>
+  );
 };
 
 export default RaceJoin;
