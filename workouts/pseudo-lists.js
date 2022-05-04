79d9252bb0501e57f105461d09ac433721ca0372
bradwray <bradwray@gmail.com>
2022-05-04 12:51:03 -0400

    fixed error in pseudo-lists workout

--- workouts/pseudo-lists.js
+++ workouts/pseudo-lists.js
@@ -1,136 +1,136 @@
 /* eslint-disable import/no-anonymous-default-export */
 export default [
-   {
-      content: `
+  {
+    content: `
       var nums = new PseudoArray([(##)])
 
        `,
-      solveFor: 'nums.getElem(2)',
-   },
-   {
-      content: `
+    solveFor: "nums.getElem(2)",
+  },
+  {
+    content: `
       var nums = new PseudoArray([(##)])
 
        `,
-      solveFor: 'nums.getElem(1)',
-   },
-   {
-      content: `
+    solveFor: "nums.getElem(1)",
+  },
+  {
+    content: `
       var nums = new PseudoArray([(##)])
       nums.setElem(1, ##)
        `,
-      solveFor: 'nums.getElem(1)',
-   },
-   {
-      content: `
+    solveFor: "nums.getElem(1)",
+  },
+  {
+    content: `
       var names = new PseudoArray([^^,^^,^^,^^,^^,^^,^^,^^,^^,^^])
 
        `,
-      solveFor: 'names.getElem(##)',
-   },
-   {
-      content: `
+    solveFor: "names.getElem(##)",
+  },
+  {
+    content: `
       var names = new PseudoArray([^^,^^,^^,^^,^^,^^,^^,^^,^^,^^])
       var chosen = ##;
       names.setElem(chosen, ^^)
-      name.setElem(##, ^^)
+      names.setElem(##, ^^)
        `,
-      solveFor: 'names.getElem(chosen)',
-   },
-   {
-      content: `
+    solveFor: "names.getElem(chosen)",
+  },
+  {
+    content: `
       var names = new PseudoArray([(^^)])
 
        `,
-      solveFor: 'names.LENGTH(names)',
-   },
-   {
-      content: `
+    solveFor: "names.LENGTH(names)",
+  },
+  {
+    content: `
       var nums = new PseudoArray([##,##,##])
       nums.setElem(nums.LENGTH(nums), ##)
        `,
-      solveFor: 'nums.getElem(3)',
-   },
-   {
-      content: `
+    solveFor: "nums.getElem(3)",
+  },
+  {
+    content: `
       var names = new PseudoArray([(^^)])
       names.REMOVE(names, names.LENGTH(names))
        `,
-      solveFor: 'names.LENGTH(names)',
-   },
-   {
-      content: `
+    solveFor: "names.LENGTH(names)",
+  },
+  {
+    content: `
       var animals = new PseudoArray([(@@)])
       animals.REMOVE(animals, 1)
        `,
-      solveFor: 'animals.getElem(1)',
-   },
-   {
-      content: `
+    solveFor: "animals.getElem(1)",
+  },
+  {
+    content: `
       var animals = new PseudoArray([(@@)])
       animals.REMOVE(animals, 2)
        `,
-      solveFor: 'animals.getElem(2)',
-   },
-   {
-      content: `
+    solveFor: "animals.getElem(2)",
+  },
+  {
+    content: `
       var animals = new PseudoArray([(@@)])
       animals.APPEND(animals, @@)
        `,
-      solveFor: 'animals.LENGTH(animals)',
-   },
-   {
-      content: `
+    solveFor: "animals.LENGTH(animals)",
+  },
+  {
+    content: `
       var animals = new PseudoArray([@@])
       animals.APPEND(animals, @@)
       animals.APPEND(animals, @@)
        `,
-      solveFor: 'animals.getElem(2)',
-   },
-   {
-      content: `
+    solveFor: "animals.getElem(2)",
+  },
+  {
+    content: `
       var animals = new PseudoArray([(@@)])
       animals.APPEND(animals, @@)
       animals.REMOVE(animals, 1)
       animals.APPEND(animals, @@)
        `,
-      solveFor: 'animals.getElem(2)',
-   },
-   {
-      content: `
+    solveFor: "animals.getElem(2)",
+  },
+  {
+    content: `
       var chars = new PseudoArray([~~,~~,~~,~~,~~])
       chars.INSERT(chars, 1, ~~)
        `,
-      solveFor: 'chars.getElem(2)',
-   },
-   {
-      content: `
+    solveFor: "chars.getElem(2)",
+  },
+  {
+    content: `
       var chars = new PseudoArray([~~,~~,~~,~~,~~])
       chars.INSERT(chars, chars.LENGTH(chars), ~~)
        `,
-      solveFor: 'chars.getElem(6)',
-   },
-   {
-      content: `
+    solveFor: "chars.getElem(6)",
+  },
+  {
+    content: `
       var chars = new PseudoArray([~~,~~,~~,~~,~~])
       chars.INSERT(chars, 1, ~~)
       chars.APPEND(chars, ~~)
       chars.REMOVE(chars, 2)
        `,
-      solveFor: 'chars.getElem(5)',
-   },
-   {
-      content: `
+    solveFor: "chars.getElem(5)",
+  },
+  {
+    content: `
       var chars = new PseudoArray([~~,~~,~~,~~,~~,~~,~~,~~,~~,~~,~~])
       chars.INSERT(chars, ##, ~~)
       chars.APPEND(chars, ~~)
       chars.REMOVE(chars, ##)
       chars.setElem(##, ~~)
        `,
-      solveFor: 'chars.getElem(5)',
-   },
-   {
-      content: `
+    solveFor: "chars.getElem(5)",
+  },
+  {
+    content: `
       var nums = new PseudoArray([##,##,##,##,##,##,#-#,##,#-#])
       nums.INSERT(nums, ##, 2)
       var value = 0;
@@ -138,10 +138,10 @@ export default [
          value = nums.getElem(##);
       }
        `,
-      solveFor: 'value',
-   },
-   {
-      content: `
+    solveFor: "value",
+  },
+  {
+    content: `
       var nums = new PseudoArray([##,##,##,##,##,##,#-#,##,#-#])
       nums.INSERT(nums, ##, 2)
       var value = 0;
@@ -152,10 +152,10 @@ export default [
          value = nums.getElem(9);
       }
        `,
-      solveFor: 'value',
-   },
-   {
-      content: `  
+    solveFor: "value",
+  },
+  {
+    content: `  
       var names = new PseudoArray([^^,^^,^^])
       
       names.APPEND(names, ^^)
@@ -169,6 +169,6 @@ export default [
   names.setElem(4, ^^)
   names.setElem(5, ^^)
        `,
-      solveFor: 'names.getElem(##)',
-   },
+    solveFor: "names.getElem(##)",
+  },
 ];
